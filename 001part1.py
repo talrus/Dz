325f447b15fa1a245abaeea395109950143cbc88
talrus <ruslan.stepanovych@gmail.com>
2019-11-22 01:24:34 +0200

    forget

---
+++ 001part1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number:\n"))
+b = int(input("Enter second number:\n"))
+print("a + b =",a + b)
+print("a - b =",a - b)
+print("a * b =",a * b)
+print("a / b =",a / b)
---
+++ 001part2.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Where do you live? ")
+print(f"Hello, {name}. Your age is {age}. You live in {city}.")
